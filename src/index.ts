2e93058d118a842426cabdd79f0489d9f5f36490
yashpal singh <yashpalsingh9rx@outlook.com>
2024-03-31 23:58:03 +0530

    add room in Websocket

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,49 @@
-import express from 'express'
-import {createServer} from 'http'
-import {WebSocketServer} from 'ws'
+import express from "express";
+import http from "http";
+import { WebSocketServer } from "ws";
+
 const app = express();
+const port = 8000;
 
-const server = createServer(app);
+const server = http.createServer(app);
 
-const wss = new WebSocketServer({server})
+const wss = new WebSocketServer({ server });
 
-wss.on("connection",async (ws,req)=>{
-    ws.on("message",(message)=>{
-        console.log("received ", message)
-        ws.send(`hello, you send ${message}`)
+const users: { [key: string]: {
+        room: string;
+        ws: any;
+    } } = {};
+
+let counter = 0;
+
+wss.on("connection", async (ws, req) => {
+    const wsId = counter++;
+    ws.on("message", (message: string) => {
+
+        const data = JSON.parse(message.toString());
+        if (data.type === "join") {
+            users[wsId] = {
+                room: data.payload.roomId,
+                ws
+            };
+        }
+
+        if (data.type === "message") {
+            const roomId = users[wsId].room;
+            const message = data.payload.message;
+
+            Object.keys(users).forEach((wsId) => {
+                if (users[wsId].room === roomId) {
+                    users[wsId].ws.send(JSON.stringify({
+                        type: "message",
+                        payload: {
+                            message
+                        }
+                    }));
+                }
+            })
+        }
     });
-})
-app.get("/health",(req,res)=>{
-    res.json({msg:"you request health endPoint"})
-})
-server.listen(8000,()=>{
-    console.log('start')
-})
+});
 
+server.listen(port);
